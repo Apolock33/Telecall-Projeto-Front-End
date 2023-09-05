1d799db4c40b56ef75455d871ede9fc3360b0416
Apolock33 <carlosammgomes@gmail.com>
2023-09-05 11:59:44 -0300

    Escopo do projeto alterado

--- Back-End/Controllers/ErrorController.php
+++
@@ -1,7 +0,0 @@
-<?php
-class ErrorController{
-    public function ErrorAction(){
-        echo 'Ocorreu um erro: a pagina requeridanão existe!';
-    }
-}
-?>
--- Back-End/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once('config.php');
-
-$connection = new Config();
-$connection->config();
-
-$action = isset($_GET['action']) ? $_GET['action'] : null;
