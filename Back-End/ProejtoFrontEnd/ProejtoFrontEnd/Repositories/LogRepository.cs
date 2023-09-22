9cac2900dfb548b6d7b08ee763d727f9d7aab5cd
Apolock33 <carlosammgomes@gmail.com>
2023-09-22 12:40:25 -0300

    Repository e service de logs implementado

---
+++ Back-End/ProejtoFrontEnd/ProejtoFrontEnd/Repositories/LogRepository.cs
@@ -0,0 +1,13 @@
+﻿using ProejtoFrontEnd.Models;
+using ProejtoFrontEnd.Repositories.Interfaces;
+using ProjetoFrontEnd_BackEnd.Models.Context;
+
+namespace ProejtoFrontEnd.Repositories
+{
+    public class LogRepository : Repository<Log>, ILogRepository
+    {
+        public LogRepository(AppDbContext db) : base(db)
+        {
+        }
+    }
+}
---
+++ Back-End/ProejtoFrontEnd/ProejtoFrontEnd/Services/LogService.cs
@@ -0,0 +1,14 @@
+﻿using ProejtoFrontEnd.Repositories.Interfaces;
+using ProejtoFrontEnd.Services.Interfaces;
+
+namespace ProejtoFrontEnd.Services
+{
+    public class LogService : ILogService
+    {
+        private readonly ILogRepository _logRepository;
+        public LogService(ILogRepository logRepository)
+        {
+            _logRepository = logRepository;
+        }
+    }
+}
