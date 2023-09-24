ed898b8eb7215687e9355d9e3757858898bb57b1
Apolock33 <carlosammgomes@gmail.com>
2023-09-24 16:59:06 -0300

    Rotas funcionais

--- Front-End/projetofrontend-client/src/App.jsx
+++ Front-End/projetofrontend-client/src/App.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const App = () => {
-
   return (
     <React.Fragment>
-      
+      <h1>Ola mundo</h1>
     </React.Fragment>
-  )
+  );
 }
 
-export default App
+export default App;
